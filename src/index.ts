56e68303e6a75c78632afb896a90c1ce8e1c8822
Joel Cares <help@joelcares.net>
2022-09-07 10:38:44 -0500

    removing dot env

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,22 @@ import * as NounsDAO from "./contracts/NounsDAO";
 import * as NounsAuctionHouse from "./contracts/NounsAuctionHouse";
 import * as NounsToken from "./contracts/NounsToken";
 import { NounsDAOABI, NounsAuctionHouseABI, NounsTokenABI} from '@nouns/contracts';
-import { sloppyABI } from "./constants";
-import { Auction, Bid, Proposal, TokenMetadata, Vote, VoteDirection, Account } from './types';
 
 
-let provider = new ethers.providers.JsonRpcProvider(process.env.JSON_RPC_API_URL);
-provider.pollingInterval = 10000;
+let provider : ethers.providers.JsonRpcProvider,
+  NounsAuctionHouseContract : ethers.Contract, 
+  NounsDAOContract : ethers.Contract, 
+  NounsTokenContract : ethers.Contract;
 
-const NounsAuctionHouseContract = new ethers.Contract( '0x830BD73E4184ceF73443C15111a1DF14e495C706' , NounsAuctionHouseABI, provider );
-const NounsDAOContract = new ethers.Contract( '0x6f3E6272A167e8AcCb32072d08E0957F9c79223d' , NounsDAOABI, provider );
-const NounsTokenContract = new ethers.Contract( '0x9C8fF314C9Bc7F6e59A9d9225Fb22946427eDC03' , NounsTokenABI, provider );
 
+export const init = async function( apiUrl : string){
+  provider = new ethers.providers.JsonRpcProvider(apiUrl);
+  provider.pollingInterval = 10000;
+
+  NounsAuctionHouseContract = new ethers.Contract( '0x830BD73E4184ceF73443C15111a1DF14e495C706' , NounsAuctionHouseABI, provider );
+  NounsDAOContract = new ethers.Contract( '0x6f3E6272A167e8AcCb32072d08E0957F9c79223d' , NounsDAOABI, provider );
+  NounsTokenContract = new ethers.Contract( '0x9C8fF314C9Bc7F6e59A9d9225Fb22946427eDC03' , NounsTokenABI, provider );
+}
 
 export const on = async function( eventName: string, listener: Function){
   console.log("StateOfNouns.ts on(" + eventName + ") created");
