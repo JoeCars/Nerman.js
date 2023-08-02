6c23b0f619771b4b3e6b11505db9906e53ed2968
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2023-08-02 10:03:26 -0700

    Fleshed out Federation functions.

--- src/utility/FederationNounsPool.ts
+++ src/utility/FederationNounsPool.ts
@@ -3,10 +3,11 @@ import NounsPool from "../contracts/Federation/NounsPool";
 import NounsPoolV2 from "../contracts/Federation/NounsPoolV2";
 import { EventData } from "../types";
 
-class FederationNounsPool {
+export class FederationNounsPool {
 	provider: ethers.providers.WebSocketProvider;
 	nounsPoolContractV1: ethers.Contract;
 	nounsPoolContractV2: ethers.Contract;
+	registeredListeners: Map<string, ethers.providers.Listener>;
 
 	constructor(jsonRpcToken: string) {
 		this.provider = new ethers.providers.WebSocketProvider(jsonRpcToken);
@@ -16,10 +17,43 @@ class FederationNounsPool {
 			NounsPoolV2,
 			this.provider
 		);
+		this.registeredListeners = new Map();
 	}
 
-	on(event: string, listener: (data: unknown) => void) {
-		this.nounsPoolContractV1.on(event, listener);
-		this.nounsPoolContractV2.on(event, listener);
+	on(event: string, listener: ethers.providers.Listener) {
+		if (event === "BidPlaced") {
+			this.nounsPoolContractV1.on(event, (dao, propId, support, amount, bidder) => {
+				listener({ dao, propId, support, amount, bidder });
+			});
+			this.nounsPoolContractV2.on(event, (dao, propId, support, amount, bidder, reason?) => {
+				listener({ dao, propId, support, amount, bidder, reason });
+			});
+		} else if (event === "VoteCast") {
+			this.nounsPoolContractV1.on(event, (dao, propId, support, amount, bidder) => {
+				listener({ dao, propId, support, amount, bidder });
+			});
+			this.nounsPoolContractV2.on(event, (dao, propId, support, amount, bidder) => {
+				listener({ dao, propId, support, amount, bidder });
+			});
+		}
+
+		this.registeredListeners.set(event, listener);
+	}
+
+	off(event: string) {
+		const listener = this.registeredListeners.get(event);
+		if (listener) {
+			this.nounsPoolContractV1.off(event, listener);
+			this.nounsPoolContractV2.off(event, listener);
+		}
+
+		this.registeredListeners.delete(event);
+	}
+
+	trigger(event: string, data: unknown) {
+		const listener = this.registeredListeners.get(event);
+		if (listener) {
+			listener(data);
+		}
 	}
 }
