2d9fd85f5c06c09885aef8f37bd13dea9336093f
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2023-08-17 16:55:09 -0700

    Index events and create loading graphic.
    Including:
    - NewImplementation
    - ProposalCanceled
    - ProposalExecuted
    - ProposalQueued
    - ProposalVetoed
    - VoteCast
    - VotingDelaySet
    - VotingPeriodSet

--- src/utility/indexer.ts
+++ src/utility/indexer.ts
@@ -5,13 +5,13 @@ import { appendFile } from "fs/promises";
 const NOUNS_STARTING_BLOCK = 13072753;
 
 async function getEvents(eventName: string, parser: (event: ethers.Event) => object, startingBlock = NOUNS_STARTING_BLOCK) {
-	const filePath = `../data/index/${eventName}.json`;
+	const filePath = `${__dirname}/../data/index/${eventName}.json`;
 
 	const nouns = new nerman.Nouns(process.env.ALCHEMY_URL);
 
 	let currentBlock = await nouns.provider.getBlockNumber();
 
-	const BLOCK_BATCH_SIZE = 2000;
+	const BLOCK_BATCH_SIZE = 1000;
 
 	await appendFile(filePath, "[");
 	let counter = 0;
@@ -32,6 +32,8 @@ async function getEvents(eventName: string, parser: (event: ethers.Event) => obj
 			await appendFile(filePath, JSON.stringify(info));
 			await appendFile(filePath, ",");
 		}
+
+		printProgress(block, startingBlock, currentBlock, eventName);
 	}
 
 	await appendFile(filePath, "]");
@@ -39,7 +41,21 @@ async function getEvents(eventName: string, parser: (event: ethers.Event) => obj
 	console.log("DONE!");
 }
 
-getEvents(process.env.NOUNS_EVENT as string, (event) => {
+function printProgress(currentBlock: number, startingBlock: number, endingBlock: number, eventName: string) {
+	let currentPercent = Math.round((100 * (currentBlock - startingBlock)) / (endingBlock - startingBlock));
+	let output = `\r${eventName} |`;
+	for (let i = 0; i < currentPercent; ++i) {
+		output += "█";
+	}
+	for (let i = currentPercent; output.length < 101; ++i) {
+		output += " ";
+	}
+	output += `| ${currentPercent}%`;
+	console.clear();
+	console.log(output);
+}
+
+function parseProposalCreatedEvent(event: ethers.Event) {
 	return {
 		blockNumber: event.blockNumber,
 		blockHash: event.blockHash,
@@ -59,6 +75,125 @@ getEvents(process.env.NOUNS_EVENT as string, (event) => {
 		signatures: event.args!.signatures,
 		calldatas: event.args!.calldatas
 	};
-}).catch((error) => {
+}
+
+function parseVoteCastEvent(event: ethers.Event) {
+	return {
+		blockNumber: event.blockNumber,
+		blockHash: event.blockHash,
+		transactionIndex: event.transactionIndex,
+		address: event.address,
+		transactionHash: event.transactionHash,
+		eventName: event.event,
+		eventSignature: event.eventSignature,
+		voterAddress: event.args!.voter,
+		proposalId: `${event.args!.proposalId}`,
+		support: event.args!.support,
+		supportChoice: ["AGAINST", "FOR", "ABSTAIN"][event.args!.support],
+		votes: `${event.args!.votes}`,
+		reason: event.args!.reason
+	};
+}
+
+function parseProposalCanceledEvent(event: ethers.Event) {
+	return {
+		blockNumber: event.blockNumber,
+		blockHash: event.blockHash,
+		transactionIndex: event.transactionIndex,
+		address: event.address,
+		transactionHash: event.transactionHash,
+		eventName: event.event,
+		eventSignature: event.eventSignature,
+		proposalId: `${event.args!.id}`,
+		status: "Cancelled"
+	};
+}
+
+function parseProposalQueuedEvent(event: ethers.Event) {
+	return {
+		blockNumber: event.blockNumber,
+		blockHash: event.blockHash,
+		transactionIndex: event.transactionIndex,
+		address: event.address,
+		transactionHash: event.transactionHash,
+		eventName: event.event,
+		eventSignature: event.eventSignature,
+		proposalId: `${event.args!.id}`,
+		eta: `${event.args!.eta}`,
+		status: "Queued"
+	};
+}
+
+function parseProposalExecutedEvent(event: ethers.Event) {
+	return {
+		blockNumber: event.blockNumber,
+		blockHash: event.blockHash,
+		transactionIndex: event.transactionIndex,
+		address: event.address,
+		transactionHash: event.transactionHash,
+		eventName: event.event,
+		eventSignature: event.eventSignature,
+		proposalId: `${event.args!.id}`,
+		status: "Executed"
+	};
+}
+
+function parseProposalVetoedEvent(event: ethers.Event) {
+	return {
+		blockNumber: event.blockNumber,
+		blockHash: event.blockHash,
+		transactionIndex: event.transactionIndex,
+		address: event.address,
+		transactionHash: event.transactionHash,
+		eventName: event.event,
+		eventSignature: event.eventSignature,
+		proposalId: `${event.args!.id}`,
+		status: "Vetoed"
+	};
+}
+
+function parseVotingDelaySetEvent(event: ethers.Event) {
+	return {
+		blockNumber: event.blockNumber,
+		blockHash: event.blockHash,
+		transactionIndex: event.transactionIndex,
+		address: event.address,
+		transactionHash: event.transactionHash,
+		eventName: event.event,
+		eventSignature: event.eventSignature,
+		oldVotingDelay: `${event.args!.oldVotingDelay}`,
+		newVotingDelay: `${event.args!.newVotingDelay}`
+	};
+}
+
+function parseVotingPeriodSetEvent(event: ethers.Event) {
+	return {
+		blockNumber: event.blockNumber,
+		blockHash: event.blockHash,
+		transactionIndex: event.transactionIndex,
+		address: event.address,
+		transactionHash: event.transactionHash,
+		eventName: event.event,
+		eventSignature: event.eventSignature,
+		oldVotingPeriod: `${event.args!.oldVotingPeriod}`,
+		newVotingPeriod: `${event.args!.newVotingPeriod}`
+	};
+}
+
+function parseNewImplementationEvent(event: ethers.Event) {
+	return {
+		blockNumber: event.blockNumber,
+		blockHash: event.blockHash,
+		transactionIndex: event.transactionIndex,
+		address: event.address,
+		transactionHash: event.transactionHash,
+		eventName: event.event,
+		eventSignature: event.eventSignature,
+		oldImplementation: `${event.args!.oldImplementation}`,
+		newImplementation: `${event.args!.newImplementation}`
+	};
+}
+
+getEvents("NewImplementation", parseNewImplementationEvent).catch((error) => {
 	console.error("Received an error.", error);
 });
