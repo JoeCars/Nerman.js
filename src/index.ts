f05d598bf084dcbac93f8155349ba8e7c1573632
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2023-10-30 16:06:30 +0400

    Refactored indexer write functions.

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ export { _NounsForkAuctionHouse as NounsForkAuctionHouse } from "./contracts/Nou
 export { _NounsFork as NounsFork } from "./contracts/NounsFork";
 export { _Propdates as Propdates } from "./contracts/propdates/Propdates";
 export { LilNouns } from "./contracts/lil-nouns/LilNouns";
+export { IndexerWriter } from "./indexer/IndexerWriter";
 
 export class Nouns {
 	public provider: ethers.providers.JsonRpcProvider;
