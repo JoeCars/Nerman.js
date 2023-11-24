7ed78dcd3ba8745c367fef79e7aba004ce9e64b6
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2023-11-24 14:08:06 -0800

    Updated parsers to convert BigNumber to number.

---
+++ scripts/update-index.ts
@@ -0,0 +1,7 @@
+import { Indexer } from "../src/index";
+import { ethers } from "ethers";
+import { join } from "path";
+
+const provider = new ethers.providers.JsonRpcProvider(process.env.ALCHEMY_URL);
+const indexer = new Indexer(provider, join(__dirname, "..", "src", "data", "indexer"));
+indexer.updateAll();
