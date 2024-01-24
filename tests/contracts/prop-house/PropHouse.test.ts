fb70e34d825394b5729184a87c5accaa96eb0dce
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2024-01-24 10:02:26 -0800

    tests: add prophouse event tests

--- tests/contracts/prop-house/PropHouse.test.ts
+++ tests/contracts/prop-house/PropHouse.test.ts
@@ -6,15 +6,30 @@ describe("PropHouse tests", () => {
 		const prophouse = new PropHouse("JSON_RPC_URL");
 
 		expect(prophouse).toBeDefined();
-		expect(prophouse.prophouse.provider).toBeDefined();
-		expect(typeof prophouse.prophouse.provider).toBe("object");
+		expect(prophouse.provider).toBeDefined();
+		expect(typeof prophouse.provider).toBe("object");
 	});
 	test("should construct with provider", () => {
-		const provider = new ethers.JsonRpcProvider("JSON_RPC_URL");
+		const provider = new ethers.providers.JsonRpcProvider("JSON_RPC_URL");
 		const prophouse = new PropHouse(provider);
 
 		expect(prophouse).toBeDefined();
-		expect(prophouse.prophouse.provider).toBeDefined();
-		expect(typeof prophouse.prophouse.provider).toBe("object");
+		expect(prophouse.provider).toBeDefined();
+		expect(typeof prophouse.provider).toBe("object");
+	});
+	test("should listen to all supported events", async () => {
+		const prophouse = new PropHouse(process.env.ALCHEMY_URL as string);
+		prophouse.provider.polling = false;
+		const mockListener = jest.fn();
+
+		try {
+			for (const eventName of PropHouse.supportedEvents) {
+				await prophouse.on(eventName, mockListener);
+				prophouse.off(eventName); // Prevents alchemy error.
+			}
+			prophouse.provider.removeAllListeners(); // Terminate provider so the tests can end.
+		} catch (error) {
+			expect(error).not.toThrow();
+		}
 	});
 });
