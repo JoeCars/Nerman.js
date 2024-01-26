546940e29a3956dedcb026c3c6a1f369c7184c66
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2024-01-26 09:02:36 -0800

    tests: require alchemy for tests

--- tests/contracts/nouns-dao/Nouns.test.ts
+++ tests/contracts/nouns-dao/Nouns.test.ts
@@ -3,14 +3,14 @@ import { ethers } from "ethers-v6";
 
 describe("Nouns tests", () => {
 	test("should construct with string", () => {
-		const nouns = new Nouns("JSON_RPC_URL", { shouldIgnoreCacheInit: true });
+		const nouns = new Nouns(process.env.ALCHEMY_URL!, { shouldIgnoreCacheInit: true });
 
 		expect(nouns).toBeDefined();
 		expect(nouns.provider).toBeDefined();
 		expect(typeof nouns.provider).toBe("object");
 	});
 	test("should construct with provider", () => {
-		const provider = new ethers.JsonRpcProvider("JSON_RPC_URL");
+		const provider = new ethers.JsonRpcProvider(process.env.ALCHEMY_URL!);
 		const nouns = new Nouns(provider, { shouldIgnoreCacheInit: true });
 
 		expect(nouns).toBeDefined();
@@ -18,8 +18,8 @@ describe("Nouns tests", () => {
 		expect(typeof nouns.provider).toBe("object");
 	});
 	test("should change polling time with option", () => {
-		const nouns1 = new Nouns("JSON_RPC_URL", { shouldIgnoreCacheInit: true, pollingTime: 1 });
-		const nouns2 = new Nouns("JSON_RPC_URL", { shouldIgnoreCacheInit: true });
+		const nouns1 = new Nouns(process.env.ALCHEMY_URL!, { shouldIgnoreCacheInit: true, pollingTime: 1 });
+		const nouns2 = new Nouns(process.env.ALCHEMY_URL!, { shouldIgnoreCacheInit: true });
 
 		expect(nouns1.provider.pollingInterval).toBe(1);
 		expect(nouns2.provider.pollingInterval).toBe(10_000);
