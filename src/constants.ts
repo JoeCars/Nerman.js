bd32bdf9ceffb0837e852aac12a701ca9875b5b7
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2024-03-18 12:42:50 -0700

    feat: add ProposalCreatedWithRequirements block and signature constants

--- src/constants.ts
+++ src/constants.ts
@@ -7,3 +7,8 @@ export const STATUS_ACTIVE = "ACTIVE";
 
 export const NOUNS_STARTING_BLOCK = 12985392;
 export const BLOCK_BATCH_SIZE = 1000;
+export const PROPOSAL_WITH_REQUIREMENTS_UPGRADE_BLOCK = 17_990_000;
+export const NEW_PROPOSAL_CREATED_WITH_REQUIREMENTS_SIGNATURE =
+	"ProposalCreatedWithRequirements(uint256,address,address[],address[],uint256[],string[],bytes[],uint256,uint256,uint256,uint256,uint256,string)";
+export const OLD_PROPOSAL_CREATED_WITH_REQUIREMENTS_SIGNATURE =
+	"ProposalCreatedWithRequirements(uint256,address,address[],uint256[],string[],bytes[],uint256,uint256,uint256,uint256,string)";
