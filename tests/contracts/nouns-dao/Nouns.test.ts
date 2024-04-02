98fa6435efce78a10658e8071037e34865c516c3
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2024-04-02 14:42:13 -0700

    tests: add calculateBookValue() test

--- tests/contracts/nouns-dao/Nouns.test.ts
+++ tests/contracts/nouns-dao/Nouns.test.ts
@@ -1,4 +1,22 @@
-import { Nouns } from "../../../src/contracts/nouns-dao/Nouns";
+import { Nouns, calculateBookValue } from "../../../src/contracts/nouns-dao/Nouns";
 import { ethers } from "ethers-v6";
 
-describe("Nouns tests", () => {});
+describe("Nouns tests", () => {
+	describe("calculateBookValue() tests", () => {
+		it("should return return 2", async () => {
+			const providerMock = {
+				getBalance() {
+					return 10n * ethers.WeiPerEther;
+				}
+			};
+			const contractMock = {
+				adjustedTotalSupply() {
+					return 5;
+				}
+			};
+
+			const result = await calculateBookValue("address", providerMock as any, contractMock as any);
+			expect(result).toEqual(2);
+		});
+	});
+});
