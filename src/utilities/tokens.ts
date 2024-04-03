02bd6b96d39b5d10248bcd4da59f2838dae90a15
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2024-04-03 13:57:12 -0700

    feat: add conversion rates to Erc20 wrappers

--- src/utilities/tokens.ts
+++ src/utilities/tokens.ts
@@ -1,6 +1,7 @@
 import { default as Erc20Abi } from "../contracts/abis/ERC20.json";
 import { ethers } from "ethers-v6";
 import { createProvider } from "./providers";
+import { WEI_PER_ETH } from "../constants";
 
 /**
  * Erc20 wrapper contract that abstracts away ABI details for a cleaner api.
@@ -26,29 +27,35 @@ export class Erc20 {
 }
 
 export class StEth extends Erc20 {
+	public static smallestDenominationRate = WEI_PER_ETH;
 	public constructor(providerOrRpcUrl: string | ethers.JsonRpcProvider) {
 		super(providerOrRpcUrl, "0xae7ab96520de3a18e5e111b5eaab095312d7fe84");
 	}
 }
 
 export class REth extends Erc20 {
+	public static smallestDenominationRate = WEI_PER_ETH;
 	public constructor(providerOrRpcUrl: string | ethers.JsonRpcProvider) {
 		super(providerOrRpcUrl, "0xae78736cd615f374d3085123a210448e74fc6393");
 	}
 }
 
 export class WEth extends Erc20 {
+	public static smallestDenominationRate = WEI_PER_ETH;
 	public constructor(providerOrRpcUrl: string | ethers.JsonRpcProvider) {
 		super(providerOrRpcUrl, "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2");
 	}
 }
 export class Usdc extends Erc20 {
+	public static smallestDenominationRate = 1_000_000;
+
 	public constructor(providerOrRpcUrl: string | ethers.JsonRpcProvider) {
 		super(providerOrRpcUrl, "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48");
 	}
 }
 
 export class WstEth extends Erc20 {
+	public static smallestDenominationRate = WEI_PER_ETH;
 	public constructor(providerOrRpcUrl: string | ethers.JsonRpcProvider) {
 		super(providerOrRpcUrl, "0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0");
 	}
