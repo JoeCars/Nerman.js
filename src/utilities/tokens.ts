d84389e700db172626cdc884122dbc076e12dd1d
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2024-04-03 13:34:13 -0700

    feat: add StEth, REth, WEth, Usdc, and WstEth contracts as children of Erc20

--- src/utilities/tokens.ts
+++ src/utilities/tokens.ts
@@ -24,3 +24,32 @@ export class Erc20 {
 		return balance;
 	}
 }
+
+export class StEth extends Erc20 {
+	public constructor(providerOrRpcUrl: string | ethers.JsonRpcProvider) {
+		super(providerOrRpcUrl, "0xae7ab96520de3a18e5e111b5eaab095312d7fe84");
+	}
+}
+
+export class REth extends Erc20 {
+	public constructor(providerOrRpcUrl: string | ethers.JsonRpcProvider) {
+		super(providerOrRpcUrl, "0xae78736cd615f374d3085123a210448e74fc6393");
+	}
+}
+
+export class WEth extends Erc20 {
+	public constructor(providerOrRpcUrl: string | ethers.JsonRpcProvider) {
+		super(providerOrRpcUrl, "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2");
+	}
+}
+export class Usdc extends Erc20 {
+	public constructor(providerOrRpcUrl: string | ethers.JsonRpcProvider) {
+		super(providerOrRpcUrl, "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48");
+	}
+}
+
+export class WstEth extends Erc20 {
+	public constructor(providerOrRpcUrl: string | ethers.JsonRpcProvider) {
+		super(providerOrRpcUrl, "0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0");
+	}
+}
