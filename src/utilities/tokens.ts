36031d53ea9d85159fb42c9d259f70d6573fb45d
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2024-04-03 15:11:52 -0700

    refactor: create Erc20 interface and change old Erc20 class to an Erc20Contract class

--- src/utilities/tokens.ts
+++ src/utilities/tokens.ts
@@ -3,10 +3,14 @@ import { ethers } from "ethers-v6";
 import { createProvider } from "./providers";
 import { WEI_PER_ETH } from "../constants";
 
+interface Erc20 {
+	balanceOf: (address: string) => Promise<bigint>;
+}
+
 /**
  * Erc20 wrapper contract that abstracts away ABI details for a cleaner api.
  */
-export class Erc20 {
+export class Erc20Contract implements Erc20 {
 	private provider: ethers.JsonRpcProvider;
 	private contract: ethers.Contract;
 
@@ -26,27 +30,27 @@ export class Erc20 {
 	}
 }
 
-export class StEth extends Erc20 {
+export class StEth extends Erc20Contract {
 	public static smallestDenominationRate = WEI_PER_ETH;
 	public constructor(providerOrRpcUrl: string | ethers.JsonRpcProvider) {
 		super(providerOrRpcUrl, "0xae7ab96520de3a18e5e111b5eaab095312d7fe84");
 	}
 }
 
-export class REth extends Erc20 {
+export class REth extends Erc20Contract {
 	public static smallestDenominationRate = WEI_PER_ETH;
 	public constructor(providerOrRpcUrl: string | ethers.JsonRpcProvider) {
 		super(providerOrRpcUrl, "0xae78736cd615f374d3085123a210448e74fc6393");
 	}
 }
 
-export class WEth extends Erc20 {
+export class WEth extends Erc20Contract {
 	public static smallestDenominationRate = WEI_PER_ETH;
 	public constructor(providerOrRpcUrl: string | ethers.JsonRpcProvider) {
 		super(providerOrRpcUrl, "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2");
 	}
 }
-export class USDC extends Erc20 {
+export class USDC extends Erc20Contract {
 	public static smallestDenominationRate = 1_000_000;
 
 	public constructor(providerOrRpcUrl: string | ethers.JsonRpcProvider) {
@@ -54,7 +58,7 @@ export class USDC extends Erc20 {
 	}
 }
 
-export class WstEth extends Erc20 {
+export class WstEth extends Erc20Contract {
 	public static smallestDenominationRate = WEI_PER_ETH;
 	public constructor(providerOrRpcUrl: string | ethers.JsonRpcProvider) {
 		super(providerOrRpcUrl, "0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0");
