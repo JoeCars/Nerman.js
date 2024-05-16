909fc3ed02ce799d594a8b6201363668facedd8f
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2024-05-16 12:12:19 -0700

    tests: fix test by renaming createProvider() to createAlchemyOrJsonRpcProvider()

--- tests/utilities/providers.test.ts
+++ tests/utilities/providers.test.ts
@@ -1,13 +1,13 @@
-import { createProvider } from "../../src/utilities/providers";
+import { createAlchemyOrJsonRpcProvider } from "../../src/utilities/providers";
 
-describe("createProvider tests", () => {
+describe("createAlchemyOrJsonRpcProvider() tests", () => {
 	it("should return an AlchemyProvider", () => {
-		const provider = createProvider("alchemy/super-secret-token");
+		const provider = createAlchemyOrJsonRpcProvider("alchemy/super-secret-token");
 		expect((provider as any).apiKey).toBeDefined(); // apiKey is unique to alchemy.
 	});
 
 	it("should return a JsonRpcProvider", () => {
-		const provider = createProvider("super-secret-token");
+		const provider = createAlchemyOrJsonRpcProvider("super-secret-token");
 		expect((provider as any).apiKey).not.toBeDefined();
 	});
 });
