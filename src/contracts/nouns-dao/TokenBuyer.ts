54ef022bfb6d27686c5eafaa9d914b0b51a79548
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2024-09-24 15:53:50 -0700

    feat: add TokenBuyer read functions

--- src/contracts/nouns-dao/TokenBuyer.ts
+++ src/contracts/nouns-dao/TokenBuyer.ts
@@ -36,12 +36,18 @@ export default class TokenBuyer {
 	private provider: JsonRpcProvider;
 	private contract: Contract;
 	private registeredListeners: Map<SupportedEventsType, Function>;
+	private tokenBuyerViewer: TokenBuyerViewer;
 	public static readonly supportedEvents = SUPPORTED_TOKEN_BUYER_EVENTS;
 
 	constructor(provider: JsonRpcProvider | string) {
 		this.provider = createOrReturnProvider(provider);
 		this.contract = createTokenBuyerContract(this.provider);
+		this.tokenBuyerViewer = new TokenBuyerViewer(this.contract);
 		this.registeredListeners = new Map();
+	}
+
+	public get viewer() {
+		return this.tokenBuyerViewer;
 	}
 
 	public async on<T extends SupportedEventsType>(eventName: T, listener: (data: SupportedEventMap[T]) => void) {
@@ -199,5 +205,96 @@ export default class TokenBuyer {
 		}
 
 		listener(data);
+	}
+}
+
+interface TokenAmountNeededAndETHPayout {
+	[key: string]: bigint;
+}
+
+class TokenBuyerViewer {
+	private contract: Contract;
+	constructor(contract: Contract) {
+		this.contract = contract;
+	}
+
+	public async MAX_BPS(): Promise<bigint> {
+		return this.contract.MAX_BPS();
+	}
+
+	public async admin(): Promise<string> {
+		return this.contract.admin();
+	}
+
+	public async baselinePaymentTokenAmount(): Promise<bigint> {
+		return this.contract.baselinePaymentTokenAmount();
+	}
+
+	public async botDiscountBPs(): Promise<bigint> {
+		return this.contract.botDiscountBPs();
+	}
+
+	public async ethAmountPerTokenAmount(tokenAmount: bigint): Promise<bigint> {
+		return this.contract.ethAmountPerTokenAmount(tokenAmount);
+	}
+
+	public async ethNeeded(additionalTokens: bigint, bufferBPs: bigint): Promise<bigint> {
+		return this.contract.ethNeeded(additionalTokens, bufferBPs);
+	}
+
+	public async maxAdminBaselinePaymentTokenAmount(): Promise<bigint> {
+		return this.contract.maxAdminBaselinePaymentTokenAmount();
+	}
+
+	public async maxAdminBotDiscountBPs(): Promise<bigint> {
+		return this.contract.maxAdminBotDiscountBPs();
+	}
+
+	public async minAdminBaselinePaymentTokenAmount(): Promise<bigint> {
+		return this.contract.minAdminBaselinePaymentTokenAmount();
+	}
+
+	public async minAdminBotDiscountBPs(): Promise<bigint> {
+		return this.contract.minAdminBotDiscountBPs();
+	}
+
+	public async owner(): Promise<string> {
+		return this.contract.owner();
+	}
+
+	public async paused(): Promise<boolean> {
+		return this.contract.paused();
+	}
+
+	public async payer(): Promise<string> {
+		return this.contract.payer();
+	}
+
+	public async paymentToken(): Promise<string> {
+		return this.contract.paymentToken();
+	}
+
+	public async paymentTokenDecimalsDigits(): Promise<bigint> {
+		return this.contract.paymentTokenDecimalsDigits();
+	}
+
+	public async price(): Promise<bigint> {
+		return this.contract.price();
+	}
+
+	public async priceFeed(): Promise<string> {
+		return this.contract.priceFeed();
+	}
+
+	public async tokenAmountNeeded(): Promise<bigint> {
+		return this.contract.tokenAmountNeeded();
+	}
+
+	public async tokenAmountNeededAndETHPayout(): Promise<TokenAmountNeededAndETHPayout> {
+		return this.contract.tokenAmountNeededAndETHPayout();
+	}
+
+	public async tokenAmountPerEthAmount(ethAmount: bigint): Promise<bigint> {
+		return this.contract.tokenAmountPerEthAmount(ethAmount);
 	}
 }
