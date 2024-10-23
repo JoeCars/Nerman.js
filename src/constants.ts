b1da1f04244a1fb29328ec939218a9091e781393
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2024-10-23 10:37:05 -0700

    feat: remove unused consts

--- src/constants.ts
+++ src/constants.ts
@@ -5,11 +5,4 @@ export const STATUS_EXECUTED = "EXECUTED";
 export const STATUS_QUEUED = "QUEUED";
 export const STATUS_ACTIVE = "ACTIVE";
 
-export const NOUNS_STARTING_BLOCK = 12985392;
-export const BLOCK_BATCH_SIZE = 1000;
-export const PROPOSAL_WITH_REQUIREMENTS_UPGRADE_BLOCK = 17_990_000;
-export const NEW_PROPOSAL_CREATED_WITH_REQUIREMENTS_SIGNATURE =
-	"ProposalCreatedWithRequirements(uint256,address,address[],address[],uint256[],string[],bytes[],uint256,uint256,uint256,uint256,uint256,string)";
-export const OLD_PROPOSAL_CREATED_WITH_REQUIREMENTS_SIGNATURE =
-	"ProposalCreatedWithRequirements(uint256,address,address[],uint256[],string[],bytes[],uint256,uint256,uint256,uint256,string)";
 export const WEI_PER_ETH = 1_000_000_000_000_000_000;
